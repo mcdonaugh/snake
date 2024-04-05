9e640e8365f0fa4bf43759a4246d456cc4a96312
mcdonaugh <brianmcdonaugh@gmail.com>
2024-04-05 10:40:48 -0400

    Added growing snake tail

--- Assets/Script/Controllers/SnakeController.cs
+++ Assets/Script/Controllers/SnakeController.cs
@@ -6,7 +6,14 @@ namespace Snake.Controllers
     public class SnakeController : MonoBehaviour
     {
         [SerializeField] private float _gameTickTime;
+        [SerializeField] private GameObject _snakeTail;
+        private GameObject[] _snakeTailArray;
+        private Vector2 _previousHeadPosition;
 
+        private void Awake()
+        {
+            _snakeTailArray = new GameObject[315];
+        }
         private void Start()
         {
             transform.position = Vector2.zero;
@@ -27,8 +34,51 @@ namespace Snake.Controllers
             }
         }
 
-        private void MoveFoward()
+        private void SpawnTail()
         {
+            GameObject newSnakeTail = Instantiate(_snakeTail, transform.position, Quaternion.identity);
+
+            for (int i = 0; i < _snakeTailArray.Length; i++)
+            {
+                if (_snakeTailArray[i] == null)
+                {
+                    _snakeTailArray[i] = newSnakeTail;
+                    Debug.Log(_snakeTailArray[i]);
+                    break;
+                }
+            }
+        }
+
+        private void MoveTail()
+        {
+            for (int i = _snakeTailArray.Length - 1; i >= 0; i--)
+            {
+                if (_snakeTailArray[i] != null && i == 0)
+                {
+                    _snakeTailArray[i].transform.position = _previousHeadPosition;
+                }
+
+                if (_snakeTailArray[i] != null && i > 0)
+                {
+                    Vector2 ParentPosition = _snakeTailArray[i-1].transform.position;
+                    _snakeTailArray[i].transform.position = ParentPosition;
+                }
+            }
+        }
+
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.tag == "Food")
+            {
+                SpawnTail();
+            }
+        }
+        
+
+        private void MoveHead()
+        {
+            _previousHeadPosition = transform.position;      
             transform.position += transform.right;
         }
 
@@ -41,7 +91,11 @@ namespace Snake.Controllers
         {
             while(true)
             {
-                MoveFoward();
+                MoveHead();
+                Debug.Log(transform.position);
+                Debug.Log(_previousHeadPosition);
+                
+                MoveTail();
                 yield return new WaitForSeconds(_gameTickTime);
             }
         }
